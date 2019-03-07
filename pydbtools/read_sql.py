21043bb33557ba5f1ade8ef88ebc14c3c0356ffa
Karik Isichei <karik.isichei@gmail.com>
2019-03-07 14:08:17 +0000

    Moving function from read_sql to utils and fixing error in it

--- pydbtools/read_sql.py
+++ pydbtools/read_sql.py
@@ -23,25 +23,3 @@ def read_sql(sql_query, timeout=None, *args, **kwargs):
     delete_s3_object(response['s3_path']+'.metadata')
     
     return df
-
-# Two functions below stolen and altered from here:
-# https://github.com/moj-analytical-services/dataengineeringutils/blob/metadata_conformance/dataengineeringutils/pd_metadata_conformance.py
-def _pd_dtype_dict_from_metadata(athena_meta):
-    """
-    Convert the athena table metadata to the dtype dict that needs to be
-    passed to the dtype argument of pd.read_csv. Also return list of columns that pandas needs to convert to dates/datetimes
-    """
-    # see https://stackoverflow.com/questions/34881079/pandas-distinction-between-str-and-object-types
-    
-    parse_dates = []
-    dtype = {}
-
-    for c in athena_meta:
-        colname = c["name"]
-        coltype = c["type"]
-        coltype = _athena_meta_conversions[coltype]['pandas']
-        dtype[colname] = np.typeDict[coltype]
-        if coltype in ["date", "timestamp"]:
-            parse_dates.append(colname)
-
-    return (dtype, parse_dates)
--- pydbtools/utils.py
+++ pydbtools/utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # Some notes on the below:
 # - int and bigint: pandas doesn't allow nulls in int columns so have to use float
 # - date and datetime: pandas doesn't really have a datetime type it expects datetimes use parse_dates
@@ -39,3 +41,24 @@ _athena_meta_conversions = {
         "pandas": "float"
     }
 }
+
+# Two functions below stolen and altered from here:
+# https://github.com/moj-analytical-services/dataengineeringutils/blob/metadata_conformance/dataengineeringutils/pd_metadata_conformance.py
+def _pd_dtype_dict_from_metadata(athena_meta):
+    """
+    Convert the athena table metadata to the dtype dict that needs to be
+    passed to the dtype argument of pd.read_csv. Also return list of columns that pandas needs to convert to dates/datetimes
+    """
+    # see https://stackoverflow.com/questions/34881079/pandas-distinction-between-str-and-object-types
+    
+    parse_dates = []
+    dtype = {}
+
+    for c in athena_meta:
+        colname = c["name"]
+        coltype = _athena_meta_conversions[c["type"]]['pandas']
+        dtype[colname] = np.typeDict[coltype]
+        if c["type"] in ["date", "timestamp"]:
+            parse_dates.append(colname)
+
+    return (dtype, parse_dates)
