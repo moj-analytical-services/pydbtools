6e620bbbaecc2b7fc67716480b8aeba9339a7ecf
Karik Isichei <karik.isichei@gmail.com>
2019-06-13 10:48:51 +0100

    Added docstring to get_athena_query_response

--- pydbtools/get_athena_query_response.py
+++ pydbtools/get_athena_query_response.py
@@ -9,6 +9,28 @@ from botocore.credentials import InstanceMetadataProvider, InstanceMetadataFetch
 def get_athena_query_response(
     sql_query, return_athena_types=False, timeout=None, force_ec2=False
 ):
+    """
+    Runs an SQL query against our Athena database and returns a tuple.
+    The first argument is an S3 path to the resulting output and the second
+    is a dictionary of meta data for the table.
+
+    sql_query: String with the SQL query you want to run against our athena
+    databases.
+
+    return_athena_types: Boolean specifying if the column definitions in the
+    meta data should be named after the athena types (True) or as our agnostic
+    meta data types used in etl_manager (False). Default is False.
+
+    timeout: Integer specifying the number of seconds to wait before giving up
+    on the Athena query. If set to None (default) the query will wait
+    indefinitely.
+
+    force_ec2: Boolean specifying if the user wants to force boto to get the
+    credentials from the EC2. This is for dbtools which is the R wrapper that
+    calls this package via reticulate and requires credentials to be refreshed
+    via the EC2 instance (and therefore sets this to True) - this is not
+    necessary when using this in Python. Default is False.
+    """
 
     # Get role specific path for athena output
     bucket = "alpha-athena-query-dump"
