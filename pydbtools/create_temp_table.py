a0121596962f61999fd73c29c65eb75bc33799cd
GeorgeKelly <george.kelly@digital.justice.gov.uk>
2020-07-20 16:19:33 +0100

    function basics

--- pydbtools/create_temp_table.py
+++ pydbtools/create_temp_table.py
@@ -2,6 +2,9 @@ import sqlparse
 from pydbtools.utils import (
     get_user_id_and_table_dir # plan to use this for creating temp db name
 )
+from pydbtools.read_sql import read_sql
+
+username, _ = get_user_id_and_table_dir()
 
 def check_sql(sql_query: str):
     """
@@ -16,17 +19,17 @@ def check_sql(sql_query: str):
             raise ValueError("The sql statement must be a select query")
 
 
-def add_user_to_temp(sql_query, username):
+def add_user_to_temp(sql_query):
     """
     add alpha username to temp database name when user queries a table
     """
-    return sql_query.lower().replace("from __temp__." f"from {username}.")
+    return sql_query.lower().replace("from __temp__.", f"from {username}.")
 
 def create_database():
     """
     create athena database with temp name, pass if already exists
-    """
-    pass
+    """  
+    read_sql(f"create database if exists {username}")
 
 def create_temp_table(table_name):
     """
