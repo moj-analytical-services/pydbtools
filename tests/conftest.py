fd70fdeee53673bc33f85305e54eac0072a60bdb
Karik Isichei <karik.isichei@gmail.com>
2021-06-24 19:26:57 +0100

    bug fix for pydbtools

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+import os
+# Get all SQL files in a dict
+@pytest.fixture(scope="module")
+def sql_dict():
+    sql_dict = {}
+    for fn in os.listdir(f"tests/data/"):
+        if fn.endswith(".sql"):
+            with open(os.path.join(f"tests/data/", fn)) as f:
+                sql_dict[fn.split(".")[0]] = "".join(f.readlines())
+    return sql_dict
