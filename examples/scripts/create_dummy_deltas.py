c137e8c0e9b6a8c05fd6cad891f4db8cc4947ac9
Mike Ratford <mike.ratford@googlemail.com>
2021-12-16 14:15:33 +0000

    Black formatting

--- examples/scripts/create_dummy_deltas.py
+++ examples/scripts/create_dummy_deltas.py
@@ -6,7 +6,9 @@ def get_dummy_deltas(employees_path):
 
     # Create deleted flag
     employees["record_deleted"] = False
-    employees["record_deleted"] = employees["record_deleted"].astype(pd.BooleanDtype())
+    employees["record_deleted"] = employees["record_deleted"].astype(
+        pd.BooleanDtype()
+    )
 
     # Cast to new int cols
     for col in ["employee_id", "department_id", "manager_id"]:
@@ -16,15 +18,20 @@ def get_dummy_deltas(employees_path):
     for col in ["sex", "forename", "surname"]:
         employees[col] = employees[col].astype(pd.StringDtype())
 
-
     # Let's split up the data and make some changes
-    day1 = employees[employees.employee_id.isin([1,2,3,4,5])].reset_index(drop=True)
+    day1 = employees[employees.employee_id.isin([1, 2, 3, 4, 5])].reset_index(
+        drop=True
+    )
 
-    day2 = employees[employees.employee_id.isin([5,6,7])].reset_index(drop=True)
+    day2 = employees[employees.employee_id.isin([5, 6, 7])].reset_index(
+        drop=True
+    )
     day2.loc[0, "department_id"] = 2
     day2.loc[0, "manager_id"] = 18
 
-    day3 = employees[employees.employee_id.isin([1,7,9,10,11])].reset_index(drop=True)
+    day3 = employees[
+        employees.employee_id.isin([1, 7, 9, 10, 11])
+    ].reset_index(drop=True)
     day3.department_id = 2
     day3.manager_id = 5
 
@@ -32,11 +39,7 @@ def get_dummy_deltas(employees_path):
     day3.loc[0, "record_deleted"] = True
     day3.loc[0, "department_id"] = 1
     day3.loc[0, "manager_id"] = 17
-    
-    deltas = {
-        "day1": day1,
-        "day2": day2,
-        "day3": day3
-    }
+
+    deltas = {"day1": day1, "day2": day2, "day3": day3}
 
     return deltas
