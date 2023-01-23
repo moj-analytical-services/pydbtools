8f2431f3dbdef13eb45bb50125d902c6d93bdbce
Gustav Moller <65606851+gustavmoller@users.noreply.github.com>
2023-01-23 13:48:11 +0000

    added show_tables function calling wr.catalog.tables

--- pydbtools/__init__.py
+++ pydbtools/__init__.py
@@ -21,6 +21,7 @@ from ._wrangler import (  # noqa: F401
     delete_database_and_data,
     save_query_to_parquet,
     dataframe_to_temp_table,
+    show_tables
 )
 
 from ._sql_render import (  # noqa: F401
