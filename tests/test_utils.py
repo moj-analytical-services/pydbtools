83edc1be88e41552f730fc15aabf2ade48d1beb8
gwionap <gwion.aprhobat@justice.gov.uk>
2023-11-28 16:55:34 +0000

    Updated get_database_name_from_userid to apply specific rules to user ids.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
-import toml
 import pytest
+import toml
 
 
 def test_set_module_values():
@@ -18,7 +18,7 @@ def test_set_module_values():
     assert pydb.utils.temp_database_name_prefix == "test_"
     assert pydb.utils.aws_default_region == "eu-west-2"
 
-    assert pydb.utils.get_database_name_from_userid("bob") == "test_bob"
+    assert pydb.utils.get_database_name_from_userid("1234") == "test_1234"
 
 
 def test_pyproject_toml_matches_version():
@@ -33,14 +33,21 @@ def test_pyproject_toml_matches_version():
     "test_input, expected",
     [
         ("abcde:12345", "12345"),
-        ("abcde:my-name", "my_name"),
+        ("abcde:my-name", None),
+        ("d347875:alpha_user_some_user", "some_user"),
         ("abcde:my-name@digital.justice.gov.uk", "my_name"),
+        ("f7fd6d7d-airflow_prod_role_name", "airflow_prod_role_name"),
     ],
 )
 def test_clean_user_id(test_input, expected):
     import pydbtools as pydb
 
-    assert (
-        pydb.utils.get_database_name_from_userid(test_input)
-        == f"{pydb.utils.temp_database_name_prefix}{expected}"
-    )
+    if expected is None:
+        with pytest.raises(ValueError):
+            pydb.utils.get_database_name_from_userid(test_input)
+
+    else:
+        assert (
+            pydb.utils.get_database_name_from_userid(test_input)
+            == f"{pydb.utils.temp_database_name_prefix}{expected}"
+        )
