fa38785e9650ada5181afaae08712dbfafd19019
Thomas-Hirsch <37899506+Thomas-Hirsch@users.noreply.github.com>
2024-07-30 16:58:17 +0100

    bump version and readme

--- pydbtools/utils.py
+++ pydbtools/utils.py
@@ -29,7 +29,8 @@ else:
     except KeyError:
         raise KeyError(
             f"""The AWS region is set to {aws_default_region}
-            but environment variable ATHENA_QUERY_DUMP_BUCKET was not set."""
+            but environment variable ATHENA_QUERY_DUMP_BUCKET was not set.
+            Either set AWS_ATHENA_QUERY_REGION to eu-west-1 or specify the query dump bucket"""
         )
 
 aws_role_regex_rules = [
